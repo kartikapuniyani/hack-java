6e40dea058908edce655e33089b7064d7b752c24
a36171 <kartika.puniyani@cars24.com>
2025-03-21 13:40:01 +0530

    initial commit

---
+++ src/main/java/hack_java/hack_java/enums/Type.java
@@ -0,0 +1,13 @@
+package hack_java.hack_java.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum Type {
+
+    HOLE,
+
+    SPEED_BREAKER;
+}
